69287d7cb1e8b52d2714dbf8e4467687715162ee
unishAryal <unish@wylight.technology>
2025-05-11 01:20:04 -0400

    removing secrets

--- backend/services/geminiPrompting.js
+++ backend/services/geminiPrompting.js
@@ -1,10 +1,11 @@
 
 import GoogleGenerativeAI from "@google/genai";
+import 'dotenv/config';
 
 // Function to generate AI content based on a prompt
 async function generateAIResponse(prompt) {
     // Initialize the generative AI model with your API key
-    const genAI = new GoogleGenerativeAI("AIzaSyChDC86eB5GapNTIT6K-4gaLvTr9YafvJU");
+    const genAI = new GoogleGenerativeAI(process.env.geminiApiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
     try {
